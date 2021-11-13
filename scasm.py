3686a5d674cd236cacd4ceeab2498d48a94a9f54
Charlie Jenkins <cjenkins72@gatech.edu>
2021-11-12 22:03:47 -0500

    Add beginning of the support for binop and name

--- scasm.py
+++ scasm.py
@@ -11,6 +11,9 @@ class AssignWrapper(ast.NodeTransformer):
     operations = []
     variables = {}
 
+
+    homogeneous_address = "0x0A0"
+
     def maybe_newline(self):
         """Adds a newline if it isn't the start of generated source"""
         if self._source:
@@ -28,6 +31,10 @@ class AssignWrapper(ast.NodeTransformer):
         """Append a piece of text"""
         self._source.append(text)
 
+    def comment(self, text):
+        """Append a piece of text"""
+        self._source.append(f";{text}\n")
+
     def traverse(self, node):
         """Recursively iterate through an AST"""
         if isinstance(node, list):
@@ -35,7 +42,7 @@ class AssignWrapper(ast.NodeTransformer):
                 self.traverse(item)
         else:
             super().visit(node)
-        
+
     def _write_constant(self, value):
         if isinstance(value, (float, complex)):
             raise UnsupportedOperation("floats and complex numbers not supported")
@@ -57,8 +64,10 @@ class AssignWrapper(ast.NodeTransformer):
         self.maybe_newline()
         if len(node.targets) > 1:
             raise UnsupportedOperation("Multi-assign not supported")
-        
-        self.write(f'{node.targets[0].id}:     DW ')
+
+        if node.targets[0].id not in self.labels:
+            self.write(f'{node.targets[0].id}:     DW ')
+            self.labels.add(node.targets[0].id)
 
         self.traverse(node.value)
 
@@ -72,13 +81,29 @@ class AssignWrapper(ast.NodeTransformer):
         self.write("\n") # Place newline before JUMP
         self.write(f'JUMP while_{this_while}')
 
-    # def visit_BinOp(self, node):
-    #     print('Node type: BinOp and fields: ', node._fields)
-    #     print(ast.dump(node))
-    #     self.operations += ["LOAD " + str(node.left.id), "ADD "+ str(node.right.id)]
-    #     self.generic_visit(node)
+    def visit_Homogeneous(self, node: ast.Homogeneous):
+        self.maybe_newline()
+        self.traverse(node.color)
+        self.write("\n")
+        self.write(f"OUT     {self.homogeneous_address}")
 
-with open('add_values.sca', 'r') as file:
+    def visit_Name(self, node: ast.Name):
+        if isinstance(node.ctx, ast.Load):
+            self.write(f"LOAD     {node.id}")
+
+    def visit_BinOp(self, node):
+        self.maybe_newline()
+
+        self.traverse(node.left)
+        self.write("\n")
+        if isinstance(node.op, ast.Add):
+            if isinstance(node.right, ast.Name):
+                self.write(f"ADD     {node.right.id}")
+            else:
+                self.write(f"ADDI     ")
+                self.traverse(node.right)
+
+with open('add_values.scpy', 'r') as file:
     tree = ast.parse(file.read())
 # print(ast.dump(tree))
 
@@ -93,8 +118,8 @@ print(result)
 with open('result.acm', 'w') as file:
     file.writelines(result)
 
-# import subprocess
-# return_code = subprocess.call(['python', 'pyscasm/scasm.py', '-o', 'drive', '-a', 'result.acm'])
+import subprocess
+return_code = subprocess.call(['python', 'pyscasm/scasm.py', '-o', 'drive', '-a', 'result.acm'])
 
-# if return_code != 0:
-#     print("Process did not complete cleanly, return code : ", return_code)
+if return_code != 0:
+    print("Process did not complete cleanly, return code : ", return_code)
